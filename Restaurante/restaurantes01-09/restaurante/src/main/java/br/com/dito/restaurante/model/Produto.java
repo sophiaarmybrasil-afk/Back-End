37c349f4918588525092f475190467fb546e8758
sophiaarmybrasil-afk <sophiaarmybrasil@gmail.com>
2026-09-15 09:59:56 -0400

    Add files via upload

---
+++ Restaurante/restaurantes01-09/restaurante/src/main/java/br/com/dito/restaurante/model/Produto.java
@@ -0,0 +1,38 @@
+package br.com.dito.restaurante.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private Double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
